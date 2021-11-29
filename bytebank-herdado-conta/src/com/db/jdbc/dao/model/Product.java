3b74012eee5e94c8a9562bae1d58be4b9e56713d
suele <suelen.ads.guimaraes@gmail.com>
2021-11-29 14:55:45 -0300

    Adiciona o model de produto.

--- bytebank-herdado-conta/src/com/db/jdbc/dao/model/Product.java
+++ bytebank-herdado-conta/src/com/db/jdbc/dao/model/Product.java
@@ -3,30 +3,50 @@ package com.db.jdbc.dao.model;
 public class Product {
 
 	private Long id;
-	private String name;
+	private String product_name;
 	private String description;
+	private Category category = new Category();
 
 	public Product(String name, String description) {
 		if (name != null && description != null) {
-			this.name = name;
+			this.product_name = name;
 			this.description = description;
 		} else {
 			System.out.println("Nome ou descrição estão nulos.");
 		}
+	}
 
+	public Product(String categoryName, String name, String description) {
+		this.category.setCategory_name(categoryName);
+		this.product_name = name;
+		this.description = description;
 	}
 
 	public String getName() {
-		return name;
+		return getProduct_name();
+	}
+
+	public String getProduct_name() {
+		return product_name;
 	}
 
 	public String getDescription() {
 		return description;
 	}
 
+	public String getCategory() {
+		return this.category.getCategory_name();
+	}
+
 	@Override
 	public String toString() {
-		return "Product [name=" + name + ", description=" + description + "]";
+		if (this.getCategory() == null) {
+			return "Product [product_name=" + product_name + ", description=" + description + "]";
+		} else {
+			return "Product [product_name=" + product_name + ", description=" + description + ", categoria= "
+					+ this.getCategory() + "]";
+		}
+
 	}
 
 }
