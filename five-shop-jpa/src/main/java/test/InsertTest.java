1940453c2a5eb5e7fe7bbfa59445726b065448ec
suele <suelen.ads.guimaraes@gmail.com>
2021-12-07 15:36:06 -0300

    Adiciona a classe para testar a persistencia e conexão com a base de dados criada.

--- five-shop-jpa/src/main/java/test/InsertTest.java
+++ five-shop-jpa/src/main/java/test/InsertTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import dao.CategoryDao;
+import dao.ProductDao;
+import model.CategoryModel;
 import model.ProductModel;
 
 import javax.persistence.EntityManager;
@@ -8,19 +11,30 @@ import javax.persistence.Persistence;
 
 public class InsertTest {
 	public static void main(String[] args) {
+
+		CategoryModel newCategory = new CategoryModel();
+		newCategory.setName("smartphone");
+
 		// Instancia o objeto produto e adiciona os atributos.
-		ProductModel newProduct = new ProductModel();
-		newProduct.setProductName("phone xaomi");
-		newProduct.setDescription("smartphone cor preto tela de 12");
+		ProductModel newProduct = new ProductModel("smartphone samsung", " cor dourado tela de 14", newCategory);
 
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("five-shop-persistence");
-
 		EntityManager em = factory.createEntityManager();
 
 		// Abre transação com o banco de dados.
 		em.getTransaction().begin();
 		// Persiste o dados ou seja altera um dado ou cria um novo.
-		em.persist(newProduct);
+		CategoryDao categoryDao = new CategoryDao(em);
+		ProductDao productDao = new ProductDao(em);
+
+		//categoryDao.insertCategory(newCategory);
+		//productDao.insertProduct(newProduct);
+		//System.out.println(productDao.selectProduct());
+
+		//System.out.println(productDao.findId(26L));
+
+		productDao.deleteProduct(productDao.findId(15L));
+
 		// Autoriza a alteração
 		em.getTransaction().commit();
 		// Encerra a transação.
